7b60d5a7facacb7a2b5427d484110b25d59626df
Aryan Patel <aryanpatel@Aryans-MacBook-Pro.local>
2025-11-27 19:45:40 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <canvas data-fit-to-container></canvas>
+        {/* <script type="module" src="machine-machenism.tsx"></script> */}
+      </div>
+      <h1>TIME MACHINE</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          {/* Edit <code>src/App.tsx</code> and save to test HMR */}
+        </p>
+      </div>
+      <p className="read-the-docs">
+        {/* Click on the Vite and React logos to learn more */}
+      </p>
+    </>
+  )
+}
+
+export default App
